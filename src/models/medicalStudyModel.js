6a7e4fe801f160d4f9a3674bfc9f1aca4e4fdff1
camila-valderrama <camival00@gmail.com>
2024-06-25 21:45:15 -0300

    probando endpoint

--- src/models/medicalStudyModel.js
+++ src/models/medicalStudyModel.js
@@ -2,14 +2,11 @@ const { db } = require('../database/db')
 const { DataTypes } = require('sequelize')
 
 const MedicalStudy = db.define("medical_studies", {
-    subject: { type: DataTypes.STRING },
-    message: { type: DataTypes.STRING },
+    name: { type: DataTypes.STRING },
+    description: { type: DataTypes.STRING },
+    date: { type: DataTypes.DATE },
     type: { type: DataTypes.INTEGER },
-    file: { type: DataTypes.STRING },
-    read: { 
-        type: DataTypes.BOOLEAN,
-        default: false
-    }
+    file: { type: DataTypes.STRING }
 })
 
 module.exports = MedicalStudy
