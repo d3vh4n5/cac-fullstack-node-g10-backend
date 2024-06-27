ee2ced660aa6c12d7d925dd42ca76b8ecead745d
Juan Basgall <juanangelbasgall@hotmail.com>
2024-06-27 13:50:41 -0300

    Renombrando medicla studies model parte 1

--- src/config/config.cjs
+++ src/config/config.cjs
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 
-dotenv.config({ path: `.env` });
+// dotenv.config({ path: `.env` });
+dotenv.config();
 
 const config = { 
     env: process.env.NODE_ENV ?? "dev",
